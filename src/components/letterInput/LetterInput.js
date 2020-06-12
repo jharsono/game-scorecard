b4415deca1cf1946a17ac88c282f30b5fcccd912
Joshua Harsono <jharsono@gmail.com>
2020-06-11 20:49:30 -0700

    Add cross out

--- src/components/letterInput/LetterInput.js
+++ src/components/letterInput/LetterInput.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	Flex,
 	Text,
+	Image,
 } from 'rebass';
 import {
 	Input,
 } from '@rebass/forms';
+import xActive from '../scattergoriesCard/assets/x-active.png';
+import xInactive from '../scattergoriesCard/assets/x-inactive.png';
+
 
 export const LetterInput = props => {
 	const { 
@@ -13,6 +17,17 @@ export const LetterInput = props => {
 			letterValue,
 	} = props;
 
+	const [isCrossedOut, setIsCrossedOut] = useState(false);
+  const [inputText, setInputText] = useState('');
+
+	const handleCrossOut = () => {
+    setIsCrossedOut(!isCrossedOut)
+	}
+
+	const handleInput = (inputValue) => {
+    setInputText(inputValue || '');
+  }
+	
 	return (
 		<Flex
 			alignItems="center"
@@ -34,9 +49,41 @@ export const LetterInput = props => {
 				</Flex>
 				<Flex>
 					<Input
+					  onChange={e => handleInput(e.target.value)}
+						disabled={isCrossedOut}
 						id={letterText}
 						name={letterText}
-					/>		
+						sx={{
+							fontSize: '16px',
+							textDecoration: `${isCrossedOut ? "line-through" : "none"}`,
+							borderTop: 0,
+							borderLeft: 0,
+							borderRight: 0,
+							p: '4px',
+							':focus': {
+								borderTop: 0,
+								borderLeft: 0,
+								borderRight: 0,
+								borderBottom: "1px solid white",
+								outline: 0, 
+							},
+						}} />
+						 <Flex
+          onClick={handleCrossOut}
+          alignItems="flex-end"
+          sx={{
+            visibility: inputText ? 'visible' : 'hidden',
+          }}
+        >
+        <Image
+          src={isCrossedOut ? xActive : xInactive}
+          sx={{
+            width: '30px',
+            height: '30px',
+            ml: '8px',
+          }}
+        />
+        </Flex>
 				</Flex>
 			
 		</Flex>
